ba7b2e7ae6f704179d9ef7738b8d8a303edf91c5
Vicky He <Vicky_He@epam.com>
2022-11-25 17:39:07 +0800

    initial configration

---
+++ vue-cli-project-2/vue.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  rules: [{
+    test: /\.(gif|png|jpe?g|svg)$/i,
+    use: [
+      'file-loader',
+      {
+        loader: 'image-webpack-loader',
+        options: {
+          bypassOnDebug: true, // webpack@1.x
+          disable: true, // webpack@2.x and newer
+        },
+      },
+    ],
+  }],
+});
