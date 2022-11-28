fa15561b7312d47872665f7e436bab5c1db82a84
Vicky He <Vicky_He@epam.com>
2022-11-28 17:40:47 +0800

    feat:add button compoment

--- vue-cli-project-2/.storybook/main.js
+++ vue-cli-project-2/.storybook/main.js
@@ -1,7 +1,15 @@
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    {
+      // 👇 The directory field sets the directory your stories
+      directory: '../src/stories',
+      // 👇 The titlePrefix field will generate automatic titles for your stories
+      titlePrefix: 'MyComponents',
+      // 👇 Storybook will load all files that contain the stories extension
+      files: '*.stories.*',
+    },
   ],
   "addons": [
     "@storybook/addon-links",
---
+++ vue-cli-project-2/src/components/MVButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script lang='ts'>
+export default {
+
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  },
+  components: {
+
+  },
+};
+</script>
+
+<style scoped lang="less">
+
+</style>
