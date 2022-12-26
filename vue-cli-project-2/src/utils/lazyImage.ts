f2cb1c81df1b0a3375fba9cafc80deaa0739a13b
Vicky He <Vicky_He@epam.com>
2022-12-26 16:10:08 +0800

    🐞 fix:delete unused args

--- vue-cli-project-2/src/utils/lazyImage.ts
+++ vue-cli-project-2/src/utils/lazyImage.ts
@@ -1,5 +1,5 @@
 import { DirectiveBinding } from "vue"
-import { hasIntersectionObserver,scrollParent } from './helper/dom'
+import { hasIntersectionObserver } from './helper/dom'
 
 export default {
   mounted(el: HTMLElement, binding: DirectiveBinding) {
@@ -9,7 +9,7 @@ export default {
         entries.forEach(function(entry) {
           if (entry.isIntersecting) {
             const image = entry.target;
-            const parent = scrollParent(el);
+            //const parent = scrollParent(el);
             // el.src = binding.value;
             image.setAttribute('src', binding.value)
             imageObserver.unobserve(image);
