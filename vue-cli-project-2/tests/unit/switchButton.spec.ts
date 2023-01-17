b190d9f8272ac785d01e7456b03f1f0245fb90e9
Vicky He <Vicky_He@epam.com>
2023-01-17 19:57:50 +0800

    🐞 fix:update the test

--- vue-cli-project-2/tests/unit/switchButton.spec.ts
+++ vue-cli-project-2/tests/unit/switchButton.spec.ts
@@ -1,14 +1,16 @@
 import { DOMWrapper, VueWrapper, shallowMount } from '@vue/test-utils'
 import SwitchButton from '@/components/SwitchButton.vue'
-
-
+import {store} from '@/store'
 describe('test SwitchButton', () => {
   let wrapper: VueWrapper<any>
   let button: Omit<DOMWrapper<HTMLElement>, "exists">
   const nameTags = ['RELEASE DATE', 'RATING']
   beforeEach(() => {
     wrapper = shallowMount(SwitchButton, {
-      props: { nameTags }
+      props: { nameTags },
+      global:{
+        plugins:[store]
+      }
     })
     button = wrapper.get('[data-test="sw-button"]')
 
@@ -18,7 +20,7 @@ describe('test SwitchButton', () => {
   })
 
   it('render button', () => {
-    expect(button.text()).toMatch(/RATING/)
+    expect(button.text()).toMatch(/RELEASE/)
   })
   it('active button', async () => {
     await button.trigger('click')
